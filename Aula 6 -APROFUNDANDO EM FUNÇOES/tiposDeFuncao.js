c899b3e99a0de127f8fc4924ef8158f216d13a2c
BRENO CASTRO GOMES <99375315+brenoocasstroo@users.noreply.github.com>
2022-02-13 21:53:46 -0300

    Add files via upload

---
+++ Aula 6 -APROFUNDANDO EM FUNÇOES/tiposDeFuncao.js
@@ -0,0 +1,28 @@
+// tipos de fucao
+
+// Declarativas  
+/*
+function funcao() {
+    console.log('sou uma mensagem de função Declarativa')
+ }
+ 
+ funcao (); 
+
+ */
+// expressão de funcão
+// com nomeação
+/*
+ var funcao = function() {
+    console.log('sou uma mensagem de função de expressão')
+ }
+ 
+ funcao ();  */
+
+
+ // arrow function
+ /*
+ var funcao = () => {
+    console.log('sou uma arrow function')
+ }
+ 
+ funcao (); */
